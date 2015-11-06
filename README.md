be643deeb8760a9e800b571591c8fd00d0977e9b
Recognition101 <recognition101@gmail.com>
2015-11-05 17:49:43 -0800

    feat(once) added -o option
     - Now uses -c option for the CLI command to run

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a small CLI utility that will run any command line program for each URL that is currently unread (non-archived) in your [pocket](https://getpocket.com) account.
 
-## Usage
+## Setup
 
 First, `cd` to the pocketForEach directory, and install node and all dependencies:
 
@@ -17,14 +17,29 @@ Then, run the setup process in order to authenticate (follow all instructions it
 node setupPocketForEach.js
 ```
 
-Once that is set up, you can use it simply with:
+## Usage
 
 ```
-node pocketForEach.js <command-with-placeholder>
+Usage: pocketForEach [options]
+
+  Options:
+
+    -h, --help               output usage information
+    -V, --version            output the version number
+    -o, --once [filename]    An optional JSON file that maintains a list of URLs that have been run already, and will not be used again in successive runs.
+    -c, --command [command]  The command to run for each URL. Instances of {{url}} will be replaced with each url.
 ```
 
-Where `<command-with-placeholder>` is a CLI command with `{{url}}` where you want the URL to be placed. For instance, if we wanted to echo every URL (pausing for a second between each echo), we could run:
+You must provide the `-c, --command` option, which is a CLI command with `{{url}}` where you want each URL to be inserted. For instance, if we wanted to simply echo each URL with a 1 second pause between each output, we could run:
 
 ```
-node pocketForEach.js "sleep 1 && echo '{{url}}'"
+node pocketForEach.js -c "sleep 1 && echo '{{url}}'"
 ```
+
+The `--once` option can be used to specify a JSON file that will be used to store which URLs have had a command run on them, and not allow any further commands to run on those URLs. This is useful if you run this script periodically and never want a command to execute on the same URL twice. For instance, if we run this command once:
+
+```
+node pocketForEach.js -c "echo '{{url}}'" -o ~/.pocketForEachLog.json
+```
+
+Then it will output each URL and store each URL in the file `~/.pocketForEachLog.json`. If we were to run that command again, it would not output anything, since every URL was already encountered once according to `~/.pocketForEachLog.json`.
--- pocketForEach.js
+++ pocketForEach.js
@@ -1,50 +1,80 @@
 /* globals require, console, __dirname, module, process */
 
-var exec    = require('child_process').exec;
 var Promise = require('bluebird'); //jshint ignore:line
+var cmdr    = require('commander');
 var core    = require('./shared/promise-core');
 
 var badConfMsg = 'Could not read config file, please run ' +
                  'setupPocketForEach.js!';
+var badCmdMsg  = 'Must give one argument: a command to run with ' +
+                 '{{url}} placeholders!';
 var conf = null;
+var once = {};
 
-core.readFile(core.configFilename, badConfMsg).then(function(dat) {
+cmdr.version('2.0.0')
+    .option('-o, --once [filename]', 'An optional JSON file ' +
+                'that maintains a list of URLs that have been run already, ' +
+                'and will not be used again in successive runs.')
+    .option('-c, --command [command]', 'The command to run for each URL. ' +
+                'Instances of {{url}} will be replaced with each url.')
+    .parse(process.argv);
+
+//Read "once" file
+(cmdr['once'] ? core.readFile(cmdr['once']) : Promise.resolve('{}'))
+.then(function(onceRaw) {
+    try { once = JSON.parse(onceRaw); } catch(e) { }
+
+//Read pocket config file and parse it
+}, function() {}).then(function() {
+    return core.readFile(core.configFilename, badConfMsg);
+
+}).then(function(dat) {
     return new Promise(function(yes, no) {
         try { conf = JSON.parse(dat); } catch(e) {}
-        var isOk = conf && conf['consumerKey'] && conf['access_token'];
-        return isOk ? yes() : no(badConfMsg);
+        var confOk  = conf && conf['consumerKey'] && conf['access_token'];
+        var cmdOk = !!cmdr['command'] && typeof cmdr['command'] === 'string';
+        return !confOk ? no(badConfMsg) :
+               !cmdOk  ? no(badCmdMsg)  : yes();
     });
-}).then(function() {
+
+//Make call to pocket API
+}).then(function(onceRaw) {
     return core.pocket('https://getpocket.com/v3/get', {
         'consumer_key': conf['consumerKey'],
         'access_token': conf['access_token'],
         'detailType': 'simple'
     });
+
+//Exec command line program on each returned URL
 }).then(function(dat) {
     var list = null;
-    var proc = process.argv[process.argv.length - 1];
+    var proc = cmdr['command'];
     try { list = JSON.parse(dat)['list']; } catch(e) { }
     if (!list) {
         throw new Error('Pocket returned a bad JSON block.');
     }
-    if (!proc) {
-        throw new Error('Must give one argument: a command to run with ' +
-                        '{{url}} placeholders!');
-    }
 
     return Object.keys(list).reduce(function(promise, keyNm) {
-        var curProc = proc.replace(/{{url}}/g, list[keyNm]['resolved_url']);
+        var curUrl  = list[keyNm]['resolved_url'];
+        var curProc = proc.replace(/{{url}}/g, curUrl);
+
         return promise.then(function() {
-            return new Promise(function(yes, no) {
-                exec(curProc, function(err, stdout, stderr) {
-                    if (err)    { console.error(err); }
-                    if (stdout) { console.log(stdout); }
-                    if (stderr) { console.error(stderr); }
-                    yes();
-                });
-            });
+            return !once[curUrl] ? core.exec(curProc).then(function(std) {
+                once[curUrl] = true;
+                if (std.out) { console.log(std.out); }
+                if (std.err) { console.error(std.err); }
+            }, function(err) {
+                console.error(err);
+            }) : true;
         });
+
     }, Promise.resolve());
+
+//Write "once" file
+}).then(function() {
+    var onceJson = JSON.stringify(once, null, '    ');
+    var errMsg   = 'Could not write file: ' + cmdr['once'];
+    return cmdr['once'] ? core.writeFile(cmdr['once'], onceJson, errMsg) : 1;
 
 }).then(function() {
     console.log('Pocket For-Each complete!');
